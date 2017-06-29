1e9eaea89d957bff40f3ff539eaf990209b3a311
Lucas Cavalcante <lcdaponte@gmail.com>
2017-06-29 14:37:25 -0300

    Disabled email delivery. And added some text to the page

--- routes/contact.js
+++ routes/contact.js
@@ -1,46 +1,16 @@
 
-var nodemailer = require('nodemailer');
+var sendEmail = require('nodemailer');
 
 module.exports = (app) => {
 
     app.post('/contact', (req, res) => {
         console.log(req.body);
 
-        sendEmail(req.body);
+        //sendEmail(req.body);
 
 
         res.sendStatus(200);
     });
 
 
-    function sendEmail(body) {
-
-        let name = body.name;
-        let from = body.email;
-        let message = body.message;
-        let to = 'lcdaponte@gmail.com';
-
-        var transporte = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: 'lcdaponte@gmail.com',
-                pass: 'saluc081221'
-            }
-        });
-
-        var email = {
-            from: from,
-            to: to,
-            subject: 'New Message from cvnode!',  // Um assunto bacana :-)
-            html: 'Nome:' + name +'<br/>' + 'Email:' + from + '<br/>' + 'Mensagem:' + message // O conteúdo do e-mail
-        };
-
-        transporte.sendMail(email, function(err, info){
-            if(err)
-                throw err; // Oops, algo de errado aconteceu.
-
-            console.log('Email enviado! Leia as informações adicionais: ', info);
-        });
-
-    }
 };
