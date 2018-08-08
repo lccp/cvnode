3f874e05ae0f922c04ae74a03a256d4a289ef0e0
Lucas Cavalcante <lucas.cavalcante@ifactory.com.br>
2018-08-08 10:09:22 -0300

    adding prod env variable

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
-var env = process.env.NODE_ENV || 'development';
+
+require('dotenv').load();
+
+var env = process.env.NODE_ENV || 'dev';
 
 console.log('env ****', env);
 
-if(env === 'development'){
+if(env === 'dev'){
     process.env.PORT = 3000;
     //process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    process.env.PORT = 3000;
+}else if(env === 'prod'){
+    process.env.PORT = 80;
     //process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
